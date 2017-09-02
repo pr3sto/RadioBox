ff859c06573ea5134085e73a3bb0b79a6d2d0e9b
pr3sto <pr3sto1377@gmail.com>
2017-09-02 20:37:47 +0300

    add all functionality

--- RadioBox/RadioBox/main.py
+++ RadioBox/RadioBox/main.py
@@ -1,21 +1,74 @@
 import kivy
-kivy.require('1.9.1')
+kivy.require('1.10.0')
 
-#fix bug with openGL
-from kivy import Config
-Config.set('graphics', 'multisamples', '0')
-
-#kivy components
+# kivy components
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
 from kivy.adapters.listadapter import ListAdapter
-from kivy.properties import StringProperty
+from kivy.properties import BooleanProperty, ObjectProperty
+
+# execute func by timer
+import sched, time
+import threading
 
 from stationutils import *
 from player import *
+
+
+class HoverBehavior(object):
+    """Hover behavior.
+    :Events:
+        `on_enter`
+            Fired when mouse enter the box of the widget.
+        `on_leave`
+            Fired when the mouse exit the widget
+    """
+
+    hovered = BooleanProperty(False)
+    border_point= ObjectProperty(None)
+    '''Contains the last relevant point received by the Hoverable. This can
+    be used in `on_enter` or `on_leave` in order to know where was dispatched the event.
+    '''
+
+    def __init__(self, **kwargs):
+        self.register_event_type('on_enter')
+        self.register_event_type('on_leave')
+        Window.bind(mouse_pos=self.on_mouse_pos)
+        super(HoverBehavior, self).__init__(**kwargs)
+
+    def on_mouse_pos(self, *args):
+        if not self.get_root_window():
+            return # do proceed if I'm not displayed <=> If have no parent
+        pos = args[1]
+        # next line to_widget allow to compensate for relative layout
+        inside = self.collide_point(*self.to_widget(*pos))
+        if self.hovered == inside:
+            # we have already done what was needed
+            return
+        self.border_point = pos
+        self.hovered = inside
+        if inside:
+            self.dispatch('on_enter')
+        else:
+            self.dispatch('on_leave')
+
+    def on_enter(self):
+        pass
+
+    def on_leave(self):
+        pass
+
+
+from kivy.factory import Factory
+Factory.register('HoverBehavior', HoverBehavior)
+
+
+class HoverButton(Button, HoverBehavior):
+    def __init__(self, **kwargs):
+        super(HoverButton, self).__init__(**kwargs)
 
 
 class AddNewStationPopup(Popup):
@@ -25,7 +78,7 @@ class AddNewStationPopup(Popup):
         # bindings
         Window.bind(on_dropfile=self.on_file_drop)
         self.bind(on_dismiss=self.refresh_popup_content)
-        
+
         super(AddNewStationPopup, self).__init__(**kwargs)
         # set default values
         self.refresh_popup_content()
@@ -33,7 +86,7 @@ class AddNewStationPopup(Popup):
     def add_new_station(self, name, url, *args):
         self.list_adapter.data.append(Station(name, url))
         self.dismiss()
-    
+
     def add_new_station_from_file(self, file_path, *args):
         stations = StationUtils.get_stations_list_from_file(file_path)
         self.list_adapter.data.extend(stations)
@@ -67,13 +120,19 @@ class PlayerScreen(FloatLayout):
     def __init__(self, list_adapter, **kwargs):
         self.list_adapter = list_adapter
         self.list_adapter.bind(on_selection_change=self.change_station)
-        super(PlayerScreen, self).__init__(**kwargs)  
-        
+        super(PlayerScreen, self).__init__(**kwargs)
+
         self.add_new_station_popup = None
 
         self.player = Player()
-        self.player.set_error_callback(self.show_error)   
-        self.player.set_title_recieved_callback(self.update_title)
+        self.player.set_volume(50)
+        self.player.set_error_callback(self._show_error)
+        self.player.set_title_recieved_callback(self._update_title)
+
+        self.is_playing = property()
+        self.is_playing = self.is_playing.getter(self.player.is_playing)
+
+        self.title_update_sheduler = sched.scheduler(time.time, time.sleep)
 
     def open_new_station_popup(self, *args):
         if self.add_new_station_popup is None:
@@ -88,27 +147,38 @@ class PlayerScreen(FloatLayout):
     def change_state(self):
         if self.player.is_playing():
             self.player.stop()
+            for event in self.title_update_sheduler.queue:
+                self.title_update_sheduler.cancel(event)
         else:
             self.player.play()
+            threading.Thread(target=self.update_title_launch).start()
 
-    def tmp(self):
+    def change_volume(self, volume):
+        self.player.set_volume(int(volume))
+
+    def update_title_launch(self):
+        self.title_update_sheduler.enter(5, 1, self._update_title_loop, ())
+        self.title_update_sheduler.run()
+
+    def _update_title_loop(self):
         self.player.get_stream_title()
+        self.title_update_sheduler.enter(5, 1, self._update_title_loop, ())
 
-    def show_error(self, stream_name):
+    def _show_error(self, stream_name):
         err_msg = 'Error playing stream \'{0}\''.format(stream_name)
         ErrorMessagePopup(err_msg).open()
 
-    def update_title(self, title):
-        print title
+    def _update_title(self, title):
+        self.ids.title_text.text = title
 
 
 class RadioBoxApp(App):
     def build(self):
         self.title = 'RadioBox'
-        
+
         # set app directory path
         StationUtils.radiobox_directory_path = self.user_data_dir
-        
+
         # load stations
         saved_stations = StationUtils.get_saved_stations_list()
         for i in range(len(saved_stations)):
@@ -117,34 +187,37 @@ class RadioBoxApp(App):
 
         # create list adapter
         self.list_adapter = ListAdapter(
-            data=saved_stations, 
+            data=saved_stations,
             args_converter=self.converter,
             selection_mode='single',
             allow_empty_selection=True,
             cls='ListItem'
         )
-        
+
         # create main screen
         ps = PlayerScreen(self.list_adapter)
-        
+
         # bindings
-        self.bind(on_stop=self.save_stations)       
+        self.bind(on_stop=self.save_stations_to_file)
         Window.minimum_width = 400
         Window.minimum_height = 300
         Window.size = (500, 400)
-        
+
         return ps
 
     def converter(self, row_index, row_data):
+        def delete_station(row_number):
+            self.list_adapter.data.remove(self.list_adapter.data[row_number-1])
         return {
-            'row_number': row_index+1, 
+            'row_number': row_index+1,
             'name': row_data.name,
-            'url': row_data.url
+            'url': row_data.url,
+            'f': delete_station
         }
 
-    def save_stations(self, *args):
+    def save_stations_to_file(self, *args):
         StationUtils.save_stations_list(self.list_adapter.data)
 
 
 if __name__ == "__main__":
-    RadioBoxApp().run()    
+    RadioBoxApp().run()
